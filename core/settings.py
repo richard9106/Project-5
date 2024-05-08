167a71464efb495a15a9ab9021c48517c53b8a76
Richard marles <andresmarles9106@gmail.com>
2024-05-08 10:09:35 +0000

    adding AWS ddbb configuration

--- core/settings.py
+++ core/settings.py
@@ -105,12 +105,25 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASE = {
+#     'old_default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbversewear',
+        'USER': os.environ.get("DATABASE_USER"),
+        'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
+        'HOST': os.environ.get("DATABASE_URL"),
+        'PORT': '5432',
+    },
 }
+
+
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.herokuapp.com",
