c9c0c003414a035eebb14343af7836df7738e038
Richard marles <andresmarles9106@gmail.com>
2024-06-08 08:21:53 +0000

    Feat: adding fields to users model and register to the admin

--- users/models.py
+++ users/models.py
@@ -1,3 +1,37 @@
+""" User model"""
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from memberships.models import Membership
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    """ Creating a new profile user"""
+    username = models.CharField('User Name', unique=True, max_length=100)
+    email = models.EmailField('Your Email', max_length=254, unique=True)
+    name = models.CharField('Your Name', max_length=200, blank=True, null=True)
+    last_name = models.CharField('Last Name', max_length=200, blank=True, null=True)
+    image = models.ImageField(
+        default='static/images/profile_images/default.jpg',
+        upload_to='static/images/profile_images/',
+        )
+    user_active = models.BooleanField(default=True)
+    current_membership = models.ForeignKey(Membership,on_delete=models.CASCADE)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = [
+        'email',
+        'name',
+        'last_name',
+    ]
+
+    def __str__(self):
+        return f'User name : {self.name} {self.last_name}'
+
+    def has_perm(self,perm,obj = None):
+        return True
+
+    def has_module_perms(self,app_label):
+        return True
+
+    
+
