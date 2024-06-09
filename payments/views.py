d07a3e91b36ba0db77d76af42eef9445cf18d244
Richard marles <andresmarles9106@gmail.com>
2024-06-09 10:34:21 +0000

    Edit: payment template and adding to the view

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def payment_manage(request):
+    """ View for manage the payments"""
+    return render(request, "payment_manage.html")
