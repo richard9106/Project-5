17bf78d8ca7006a37dce79270a65801a27fdcc84
Richard marles <andresmarles9106@gmail.com>
2024-06-15 11:22:22 +0000

    Feat:adding media files and creating fixtures for products and link all product links and home logo

---
+++ products/admin.py
@@ -0,0 +1,28 @@
+"""Register model to admin"""
+from django.contrib import admin
+from .models import Product, Category
+
+# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    """ List of fields to display"""
+    list_display = (
+        'sku',
+        'name',
+        'description',
+        'price',
+        'rating',
+        'image',
+    )
+    ordering = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """ List of fields to display"""
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
---
+++ products/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from cloudinary.models import CloudinaryField
+
+
+
+# Create your models here.
+class Category(models.Model):
+    """Info of the diferent categories for our products"""
+
+    class Meta:
+        """select a name to display in admin"""
+        verbose_name_plural = 'categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254)
+
+    def __str__(self):
+        """returne the name"""
+        return self.name
+
+    def get_friendly_name(self):
+        """get firendly name"""
+        return self.friendly_name
+
+
+class Product(models.Model):
+    """model for products /  articles"""
+    category =  models.ForeignKey("Category",
+                                  null=True,
+                                  blank=True,
+                                  on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
