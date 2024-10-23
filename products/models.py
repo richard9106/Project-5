6bb4d3352ab38d49e1c7b104a48d54d4361ca383
Richard marles <andresmarles9106@gmail.com>
2024-10-23 08:59:31 +0000

    Fix rating no more than 5

--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator,  MaxValueValidator
 
 
 
@@ -35,7 +35,7 @@ class Product(models.Model):
     description = models.TextField()
     has_size=models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, validators=[MinValueValidator(0)])
-    rating = models.DecimalField(max_digits=6, decimal_places=0, null=True, blank=True, validators=[MinValueValidator(0)])
+    rating = models.DecimalField(max_digits=6, decimal_places=0, null=True, blank=True, validators=[MinValueValidator(0),  MaxValueValidator(5)])
     image = CloudinaryField('image', null=True, blank=True,
                              default="static/images/profile_images/default-product-image.png")
 
