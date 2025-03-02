a2a188b1afaf0eedfc9e074b9b7c28b3baba6476
RichardM <114283158+richard9106@users.noreply.github.com>
2025-03-02 14:33:20 +0100

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', '.gitpod.io', 'console.neon.tech' ]
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', '.gitpod.io', '.neon.tech' ]
 
 
 # Application definition
